b4fd9f6073081951fe3e63790ab4f8fa24ba03e6
isRicko <92978639+isRicko@users.noreply.github.com>
2022-04-20 20:56:39 +0800

    init

---
+++ Phoebe_23812181_WanjunLuo/New Unity Project/Assets/Game04/Scripts/GameController04.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController04 : MonoBehaviour
+{
+    public static GameController04 Instance = null;
+    public int Coinscore = 0;
+    public Text Cointxt_score;
+
+    public GameObject WinPanel;
+
+    public AudioSource GetSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+    }
+
+
+
+    public void SetCoin()
+    {
+        GetSound.Play();
+        Coinscore += 1;
+
+        Cointxt_score.text = Coinscore.ToString();
+
+        if (Coinscore >= 5)
+        {
+
+            WinPanel.SetActive(true);
+
+        }
+    }
+
+    public void GameController04Again()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void GameController04Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+      Application.Quit();
+#endif
+    }
+}
---
+++ Phoebe_23812181_WanjunLuo/New Unity Project/Assets/Game04/Scripts/ManController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManController : MonoBehaviour
+{
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GameController04.Instance.SetCoin();
+            Destroy(this.gameObject);
+        }
+    }
+
+}
